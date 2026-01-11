f6bcdb60636608ae05728045164f6fd482260ffc
josedinunzio <josedinunzio@microsoft.com>
2026-01-11 11:03:07 +0000

    Refactor darwin config into common module, remove ssh known hosts

---
+++ system/config-darwin.nix
@@ -0,0 +1,34 @@
+{ ... }: {
+  
+  system.defaults = {
+    NSGlobalDomain."com.apple.swipescrolldirection" = false;
+    dock.autohide = true;
+  };
+
+
+  homebrew.casks = [
+    "alt-tab"
+    "firefox"
+    "microsoft-teams"
+    # "vscode"
+  ];
+
+  # Enable alternative shell support in nix-darwin.
+  programs.fish.enable = true;
+
+  services.openssh.enable = true;
+
+  # Set Git commit hash for darwin-version.
+  # system.configurationRevision = self.rev or self.dirtyRev or null;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 6;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  # nixpkgs.config = {
+  #   allowUnfree = true;
+  # };
+}
