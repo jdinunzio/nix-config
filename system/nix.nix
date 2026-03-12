b52699f06cea7b001c845cd073f184fd2aa6ed3e
Jose Di Nunzio <jose.dinuncio@gmail.com>
2026-03-12 20:37:13 +0000

    Add trusted substituters for nix

--- system/nix.nix
+++ system/nix.nix
@@ -28,5 +28,9 @@
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
     ];
+    trusted-substituters = [
+      "https://devenv.cachix.org"
+      "https://nixpkgs-python.cachix.org"
+    ];
   };
 }
